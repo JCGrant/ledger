03372d930aeaef4ede9aa5ccfb8114eedcde30b8
JCGrant <james@jcgrant.com>
2021-12-25 11:58:11 +0000

    fix: keep making trades until can't any more

--- server/wss.js
+++ server/wss.js
@@ -9,49 +9,65 @@ import {
 
 const wss = new WebSocketServer({ noServer: true });
 
+function getBuySellPair(orders, item) {
+  const buys = orders
+    .filter(
+      (order) =>
+        !order.completed &&
+        order.direction === "buy" &&
+        order.itemId === item.id
+    )
+    .sort((a, b) => b.price - a.price);
+  const highestBuy = buys[0];
+  if (!highestBuy) {
+    return;
+  }
+  const sells = orders
+    .filter(
+      (order) =>
+        !order.completed &&
+        order.direction === "sell" &&
+        order.itemId === item.id &&
+        order.userId !== highestBuy.userId
+    )
+    .sort((a, b) => a.price - b.price);
+  const lowestSell = sells[0];
+  if (!lowestSell) {
+    return;
+  }
+  if (highestBuy.price < lowestSell.price) {
+    return;
+  }
+  return [highestBuy, lowestSell];
+}
+
 setInterval(async () => {
-  const [items, orders] = await Promise.all([getItems(), getOrders()]);
+  const items = await getItems();
   items.forEach(async (item) => {
-    const buys = orders
-      .filter(
-        (order) =>
-          !order.completed &&
-          order.direction === "buy" &&
-          order.itemId === item.id
-      )
-      .sort((a, b) => b.price - a.price);
-    const highestBuy = buys[0];
-    if (!highestBuy) {
-      return;
+    while (true) {
+      const orders = await getOrders();
+      const pair = getBuySellPair(orders, item);
+      if (!pair) {
+        return;
+      }
+      const [highestBuy, lowestSell] = pair;
+      const updatedOrders = await Promise.all([
+        updateOrder(highestBuy.id, [["completed", true]]),
+        updateOrder(lowestSell.id, [["completed", true]]),
+      ]);
+      console.log(pair);
+      const event = {
+        type: "ORDERS_COMPLETED",
+        payload: {
+          orders: updatedOrders,
+        },
+      };
+      wss.clients.forEach((client) => client.send(JSON.stringify(event)));
+      // create transaction
+      // update user wallet
+      // create user inventory, and move the owned items to other user
+      // display wallet and items in user profiles
     }
-    const sells = orders
-      .filter(
-        (order) =>
-          !order.completed &&
-          order.direction === "sell" &&
-          order.itemId === item.id &&
-          order.userId !== highestBuy.userId
-      )
-      .sort((a, b) => a.price - b.price);
-    const lowestSell = sells[0];
-    if (!lowestSell) {
-      return;
-    }
-    const updatedOrders = await Promise.all([
-      updateOrder(highestBuy.id, [["completed", true]]),
-      updateOrder(lowestSell.id, [["completed", true]]),
-    ]);
-    const event = {
-      type: "ORDERS_COMPLETED",
-      payload: {
-        orders: updatedOrders,
-      },
-    };
-    wss.clients.forEach((client) => client.send(JSON.stringify(event)));
-    // create transaction
-    // update user wallet
-    // create user inventory, and move the owned items to other user
-    // display wallet and items in user profiles
   });
 }, 1000);
 
