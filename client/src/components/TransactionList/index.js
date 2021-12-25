4d0354f4dcf9060892b537a86c1e9c160c912bfd
David Grant <david.grant@blubolt.com>
2021-12-25 23:46:37 +0000

    update transactions page styling

--- client/src/components/TransactionList/index.js
+++ client/src/components/TransactionList/index.js
@@ -1,3 +1,5 @@
+import "./styles.scss";
+
 const Transaction = ({ id, sellOrder, buyOrder, timestamp }) => {
   const date = new Date(timestamp);
   const itemName = sellOrder.item.name;
@@ -5,22 +7,32 @@ const Transaction = ({ id, sellOrder, buyOrder, timestamp }) => {
   const buyerName = buyOrder.user.name;
   const settledPrice = (buyOrder.price + sellOrder.price) / 2;
   return (
-    <li key={id}>
-      {itemName}: {sellerName} {"->"} {buyerName}, for {settledPrice} tokens, at{" "}
-      {date.toLocaleTimeString()}
-    </li>
+    <tr className="transaction" key={id}>
+      <td>{itemName}</td>
+      <td>{sellerName}</td>
+      <td>{buyerName}</td>
+      <td>{settledPrice}</td>
+      <td>{date.toLocaleTimeString()}</td>
+    </tr>
   );
 };
 
 const TransactionList = ({ transactions }) => {
   return (
-    <div>
+    <div className="transactions container">
       <h1>Transactions</h1>
-      <ul>
+      <table className="transactions-wrapper">
+        <tr>
+          <th>Item</th>
+          <th>Seller</th>
+          <th>Buyer</th>
+          <th>Price</th>
+          <th>Time</th>
+        </tr>
         {transactions.map((transaction) => (
           <Transaction key={transaction.id} {...transaction} />
         ))}
-      </ul>
+      </table>
     </div>
   );
 };
