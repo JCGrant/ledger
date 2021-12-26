f8a69760d03b0e4e4478e2326664ca44cdbcf530
JCGrant <james@jcgrant.com>
2021-12-26 16:18:36 +0000

    feat: show other user's tokens next to name on Home page

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -6,12 +6,13 @@ const Home = ({ user, users }) => {
       <h1>Hello, {user.name}!</h1>
       <p>You have {user.numTokens} tokens</p>
       <div className="users-wrapper">
-        <h2 className="heading-2">User Profiles</h2>
+        <h2 className="heading-2">Other Users</h2>
         {users.map((user) => (
           <div className="user" key={user.id}>
             <Link className="user-link" to={`/users/${user.id}`}>
               {user.name}
-            </Link>
+            </Link>{" "}
+            - {user.numTokens} tokens
           </div>
         ))}
       </div>
